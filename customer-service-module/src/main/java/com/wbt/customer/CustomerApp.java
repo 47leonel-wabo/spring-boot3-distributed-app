4d73b89f5ae9f64554bf729130e5db4de9b4b7f4
leonel47 <waboleonel@gmail.com>
2023-06-06 11:34:02 +0100

    Configuring fraud and customer services to register to server

--- customer-service-module/src/main/java/com/wbt/customer/CustomerApp.java
+++ customer-service-module/src/main/java/com/wbt/customer/CustomerApp.java
@@ -2,8 +2,10 @@ package com.wbt.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerApp {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApp.class, args);
