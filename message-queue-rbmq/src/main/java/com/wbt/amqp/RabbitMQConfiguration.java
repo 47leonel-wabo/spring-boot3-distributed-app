71a285dde6db2b45a4d1870f038db7c1d45690cc
leonel47 <waboleonel@gmail.com>
2023-06-08 20:33:59 +0100

    Configuring rabbitmq template, listener, and message converter

---
+++ message-queue-rbmq/src/main/java/com/wbt/amqp/RabbitMQConfiguration.java
@@ -0,0 +1,38 @@
+package com.wbt.amqp;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public record RabbitMQConfiguration(ConnectionFactory connectionFactory) {
+
+    // This allows to send message to the queue
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    // This allows to receive message from the queue
+    @Bean
+    public SimpleRabbitListenerContainerFactory listenerContainerFactory() {
+        final var factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    // This allows to convert message in json format
+    @Bean
+    public MessageConverter jacksonConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
